481e1a816724e8296215105a4eb0eabb7d77cacf
daozhao <daozhao@163.com>
2014-04-17 14:20:11 +0800

    Add Xcode5 project file . you can compile DarwinSteamingServer in Xcode.

---
+++ DSSxcode5Project/DSSxcode5Project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DSSxcode5Project
+//
+//  Created by daozhao chen on 4/17/14.
+//  Copyright (c) 2014 daozhao chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
